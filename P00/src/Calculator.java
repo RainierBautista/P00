20b7eabbc6150052d092552e735292683493070b
Rainier <Rainier@10.175.7.10>
2023-05-30 17:41:21 +0800

    first push to P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,19 @@
+
+public class Calculator {
+	public int add(int a, int b) {
+	return a + b;
+	}//end of add
+	
+	public int substract(int a, int b) {
+	return a - b;  
+	}//end of subtract
+	
+	public int multiple(int a, int b) {
+		return a * b;
+	}//end of multiple
+	
+	public int divide(int a, int b) {
+		return a / b;
+	}//end of divide
+	
+}//end of class
